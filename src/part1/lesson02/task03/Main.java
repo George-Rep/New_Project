d3ba3ff3be26d6dc715fed1a79772baac32defa9
Georgii Shishkin <565-45@bk.ru>
2019-04-21 20:54:24 +0300

    изменен метод swap

--- src/part1/lesson02/task03/Main.java
+++ src/part1/lesson02/task03/Main.java
@@ -24,6 +24,8 @@ import java.util.Scanner;
  * Класс Person характеризуется полями age (возраст, целое число 0-100),
  * sex (пол – объект класса Sex , name (имя - строка). Сделан геттер
  * свойства sex из указанного выше объекта класса Sex.
+ * @see Main
+ *
  */
 class Person {
     private int age;
@@ -80,22 +82,11 @@ class Sex {
 }
 
 /**
- * класс ObjectWrapper является Wrapper'ом для класса Person,
- * сделан для того, чтобы в дальнейшем менять местами (swap) объекты Person.
- */
-class ObjectWrapper {
-    Person person;
-
-    ObjectWrapper(Person person) {
-        this.person = person;
-    }
-}
-
-/**
  * класс MathUtils
  * содержит вспомогательные функции: 1)генерация одного слова из ФИО (например фамилии)
- * 2) сравнение двух объектов Person (ObjectWrapper.person)
- * 3) swap (смена местами) двух объектов Person (ObjectWrapper.person)
+ * 2) сравнение двух объектов Person
+ * 3) swap (смена местами) двух объектов Person
+ *
  */
 class MathUtils {
     /**
@@ -119,36 +110,38 @@ class MathUtils {
     }
 
     /**
-     * сравнение двух объектов Person, в обертке (ObjectWrapper.person)
+     * сравнение двух объектов Person
      * возвращает значение больше нуля, если person1>person2; меньше нуля, если person1<person2,
      * ноль при равенстве. Под person1>person2 в данной задаче понимается в т.ч., что
      * по возрасту "старше < моложе" (см. условия задачи, "выше в списке тот, кто более старший").
      */
-    static int comparePersons(ObjectWrapper person1, ObjectWrapper person2) {
-        if (person1.person.getSex().compareTo(person2.person.getSex()) != 0)
-            return person1.person.getSex().compareTo(person2.person.getSex());
-        if (person1.person.getAge() != person2.person.getAge())
-            return person2.person.getAge() - person1.person.getAge();
+    static int comparePersons(Person person1, Person person2) {
+        if (person1.getSex().compareTo(person2.getSex()) != 0)
+            return person1.getSex().compareTo(person2.getSex());
+        if (person1.getAge() != person2.getAge())
+            return person2.getAge() - person1.getAge();
         try {
-            if (person1.person.getName().compareTo(person2.person.getName()) == 0)
+            if (person1.getName().compareTo(person2.getName()) == 0)
                 throw new Exception("Exception: same");
         } catch (Exception e) {
             System.out.println("Exception при основной сортировке" + System.getProperty("line.separator")
-                    + person1.person.getSex() + " " + person1.person.getAge() + " " + person1.person.getName()
+                    + person1.getSex() + " " + person1.getAge() + " " + person1.getName()
                     + System.getProperty("line.separator"));
         }
-        return person1.person.getName().compareTo(person2.person.getName());
+        return person1.getName().compareTo(person2.getName());
 
     }
 
     /**
-     * метод swap (смена местами) двух объектов Person в обертках (ObjectWrapper.person)
+     * метод swap (смена местами) двух объектов Person, находящихся в массиве
+     * personArray , номера элементов - arrayIndex1 и arrayIndex2
+     *
      */
-    static void Swap(ObjectWrapper person1, ObjectWrapper person2) {
+    static void Swap(Person[] personArray, int arrayIndex1, int arrayIndex2) {
         Person temp;
-        temp = person1.person;
-        person1.person = person2.person;
-        person2.person = temp;
+        temp = personArray[arrayIndex1];
+        personArray[arrayIndex1] = personArray[arrayIndex2];
+        personArray[arrayIndex2] = temp;
     }
 
 }
@@ -158,7 +151,7 @@ class MathUtils {
  * принимает массив для сортировки, отдает время работы в миллисекундах.
  */
 interface Sortingalgo {
-    long sort(ObjectWrapper[] arrayForSorting);
+    long sort(Person[] arrayForSorting);
 }
 
 /**
@@ -168,13 +161,12 @@ class BubbleSort implements Sortingalgo {
     /**
      * метод сортирует массив arrayForSorting
      */
-    public long sort(ObjectWrapper[] arrayForSorting) {
+    public long sort(Person[] arrayForSorting) {
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
         for (int i = 0; i < arrayForSorting.length - 1; i++)
             for (int j = 0; j < arrayForSorting.length - i - 1; j++)
                 if (MathUtils.comparePersons(arrayForSorting[j], arrayForSorting[j + 1]) > 0) {
-                    MathUtils.Swap(arrayForSorting[j], arrayForSorting[j + 1]);
-
+                    MathUtils.Swap(arrayForSorting, j, j + 1);
                 }
         return new Timestamp(System.currentTimeMillis()).getTime() - timestamp.getTime();
     }
@@ -187,23 +179,23 @@ class QSort implements Sortingalgo {
     /**
      * partition, метод разбиения quicksort ("элементы меньше опорного помещаются перед ним, а больше или равные после")
      */
-    int partition(ObjectWrapper[] arrayPart, int lowIndex, int highIndex) {
-        ObjectWrapper pivot = arrayPart[highIndex];
+    int partition(Person[] arrayPart, int lowIndex, int highIndex) {
+        Person pivot = arrayPart[highIndex];
         int i = (lowIndex - 1);
         for (int j = lowIndex; j < highIndex; j++) {
             if (!(MathUtils.comparePersons(arrayPart[j], pivot) > 0)) {
                 i++;
-                MathUtils.Swap(arrayPart[i], arrayPart[j]);
+                MathUtils.Swap(arrayPart, i, j);
             }
         }
-        MathUtils.Swap(arrayPart[i + 1], arrayPart[highIndex]);
+        MathUtils.Swap(arrayPart, i + 1, highIndex);
         return i + 1;
     }
 
     /**
      * сортировка части массива от lowIndex до highIndex
      */
-    private void arrayPartSort(ObjectWrapper[] arrayPart, int lowIndex, int highIndex) {
+    private void arrayPartSort(Person[] arrayPart, int lowIndex, int highIndex) {
         if (lowIndex < highIndex) {
             int partitioningIndex = partition(arrayPart, lowIndex, highIndex);
             arrayPartSort(arrayPart, lowIndex, partitioningIndex - 1);
@@ -214,7 +206,7 @@ class QSort implements Sortingalgo {
     /**
      * основной метод класса, сортирует quicksort'ом массив arrayForSorting
      */
-    public long sort(ObjectWrapper[] arrayForSorting) {
+    public long sort(Person[] arrayForSorting) {
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
         arrayPartSort(arrayForSorting, 0, arrayForSorting.length - 1);
         return new Timestamp(System.currentTimeMillis()).getTime() - timestamp.getTime();
@@ -230,28 +222,28 @@ class QSort2 extends QSort {
     /**
      * Аналогично MathUtils.comparePersons , но сортировка ведется только по именам и возрасту/
      * <p>
-     * Сравнение двух объектов Person, в обертке (ObjectWrapper.person)
+     * Сравнение двух объектов Person
      * возвращает значение больше нуля, если person1>person2; меньше нуля, если person1<person2,
      * равно нулю при равенстве. Под person1>person2 в данной задаче понимается в т.ч., что
      * по возрасту "старше < моложе" (см. условия задачи).
      */
-private    int comparePersons2(ObjectWrapper person1, ObjectWrapper person2) {
-//        if (person1.person.getSex().compareTo(person2.person.getSex()) != 0)
-//            return person1.person.getSex().compareTo(person2.person.getSex());
-        if (person1.person.getAge() != person2.person.getAge())
-            return person2.person.getAge() - person1.person.getAge();
+    private int comparePersons2(Person person1, Person person2) {
+//        if (person1.getSex().compareTo(person2.getSex()) != 0)
+//            return person1.getSex().compareTo(person2.getSex());
+        if (person1.getAge() != person2.getAge())
+            return person2.getAge() - person1.getAge();
         try {
-            if (person1.person.getName().compareTo(person2.person.getName()) == 0)
+            if (person1.getName().compareTo(person2.getName()) == 0)
                 throw new Exception("Exception: same");
         } catch (Exception e) {
             System.out.println("Exception при специальной сортировке" + System.getProperty("line.separator")
-                    + person1.person.getSex() + " " + person1.person.getAge() + " "
-                    + person1.person.getName() + System.getProperty("line.separator") +
-                    person2.person.getSex() + " " + person2.person.getAge() + " " + person2.person.getName()
+                    + person1.getSex() + " " + person1.getAge() + " "
+                    + person1.getName() + System.getProperty("line.separator") +
+                    person2.getSex() + " " + person2.getAge() + " " + person2.getName()
                     + System.getProperty("line.separator"));
 
         }
-        return person1.person.getName().compareTo(person2.person.getName());
+        return person1.getName().compareTo(person2.getName());
 
     }
 
@@ -259,16 +251,16 @@ private    int comparePersons2(ObjectWrapper person1, ObjectWrapper person2) {
      * partition, метод разбиения quicksort ("элементы меньше опорного помещаются перед ним, а больше или равные после").
      * Отличается от QSort.partition используемой функцией сравнения (здесь - comparePersons2)
      */
-    int partition(ObjectWrapper[] arrayPart, int lowIndex, int highIndex) {
-        ObjectWrapper pivot = arrayPart[highIndex];
+    int partition(Person[] arrayPart, int lowIndex, int highIndex) {
+        Person pivot = arrayPart[highIndex];
         int i = (lowIndex - 1);
         for (int j = lowIndex; j < highIndex; j++) {
             if (!(comparePersons2(arrayPart[j], pivot) > 0)) {
                 i++;
-                MathUtils.Swap(arrayPart[i], arrayPart[j]);
+                MathUtils.Swap(arrayPart, i, j);
             }
         }
-        MathUtils.Swap(arrayPart[i + 1], arrayPart[highIndex]);
+        MathUtils.Swap(arrayPart, i + 1, highIndex);
         return i + 1;
     }
 }
@@ -278,16 +270,15 @@ private    int comparePersons2(ObjectWrapper person1, ObjectWrapper person2) {
  */
 public class Main {
     /**
-     * генерируется массив объектов Person. Но, для задач смены элементов местами при сортировке он
-     * неудобен, поэтому на его основе делается соответсвующий массив объектов ObjectWrapper(.person)
-     * и сортировка ведется в последнем.
+     * генерируется массив объектов Person.
      * <p>
      * сортировка приоисходит три раза, каждый раз на вход подается копия одного и того же
      * изначально сгенерированного массива. QSort2.sort производится для поиска совпадений и
      * корректного выполнения условия "Если имена людей и возраст совпадают, выбрасывать в программе
      * пользовательское исключение.".
-     * Далее запускаются алгоритм сортировки пузырьком и Quicksort,
+     * Далее запускаются алгоритмы сортировки пузырьком и Quicksort,
      * выводится время потраченное на сортировку каждым алгоритмом.
+     *
      */
     public static void main(String[] args) {
         int N;
@@ -303,10 +294,9 @@ public class Main {
             N = inputScanner.nextInt();
         } while ((N <= 0) || (N > 50000));
 
-        Person[] personArray = new Person[N];
-        ObjectWrapper[] personWrapperArray1 = new ObjectWrapper[N];
-        ObjectWrapper[] personWrapperArray2 = new ObjectWrapper[N];
-        ObjectWrapper[] personWrapperArray3 = new ObjectWrapper[N];
+        Person[] personArray1 = new Person[N];
+        Person[] personArray2 = new Person[N];
+        Person[] personArray3 = new Person[N];
         int age;
         Sex sex;
         String name;
@@ -315,30 +305,25 @@ public class Main {
         SecureRandom secureRandom = new SecureRandom();
         for (int i = 0; i < N; ++i) {
             age = secureRandom.nextInt(101);
-/*            if (secureRandom.nextInt(2) == 1)
-                sex = new Sex(Sex.WOMAN);
-            else sex = new Sex(Sex.MAN);*/
             sex = (secureRandom.nextInt(2) == 1) ? new Sex(Sex.WOMAN) : new Sex(Sex.MAN);
             name = MathUtils.generateNamePart(maxNameWordLength) + " " + MathUtils.generateNamePart(maxNameWordLength);
-            personArray[i] = new Person(age, sex, name);
-            personWrapperArray1[i] = new ObjectWrapper(personArray[i]);
-            personWrapperArray2[i] = new ObjectWrapper(personArray[i]);
-            personWrapperArray3[i] = new ObjectWrapper(personArray[i]);
+            personArray1[i] = new Person(age, sex, name);
+            personArray2[i] = new Person(age, sex, name);
+            personArray3[i] = new Person(age, sex, name);
         }
-
         // для проверки работы исключений при совпадении
-  /*      personWrapperArray1[10].person.setName(personWrapperArray1[0].person.getName());
-        personWrapperArray1[10].person.setAge(personWrapperArray1[0].person.getAge());
-        personWrapperArray2[10].person.setName(personWrapperArray2[0].person.getName());
-        personWrapperArray2[10].person.setAge(personWrapperArray2[0].person.getAge());
-        personWrapperArray3[10].person.setName(personWrapperArray3[0].person.getName());
-        personWrapperArray3[10].person.setAge(personWrapperArray3[0].person.getAge());*/
-        new QSort2().sort(personWrapperArray1);
-        time1 = new BubbleSort().sort(personWrapperArray2);
-        time2 = new QSort().sort(personWrapperArray3);
+/*        personArray1[10].setName(personArray1[0].getName());
+        personArray1[10].setAge(personArray1[0].getAge());
+        personArray2[10].setName(personArray2[0].getName());
+        personArray2[10].setAge(personArray2[0].getAge());
+        personArray3[10].setName(personArray3[0].getName());
+        personArray3[10].setAge(personArray3[0].getAge());*/
+        new QSort2().sort(personArray1);
+        time1 = new BubbleSort().sort(personArray2);
+        time2 = new QSort().sort(personArray3);
         for (int i = 0; i < N; ++i)
-            System.out.println(personWrapperArray3[i].person.getSex() + " " + personWrapperArray3[i].person.getAge()
-                    + " " + personWrapperArray3[i].person.getName());
+            System.out.println(personArray3[i].getSex() + " " + personArray3[i].getAge()
+                    + " " + personArray3[i].getName());
         System.out.println(System.getProperty("line.separator") + "Bubblesort: " + time1 / 1000.0 + "с"
                 + System.getProperty("line.separator") + "Quicksort: " + time2 / 1000.0 + "с");
 
