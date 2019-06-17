785b69778fe81a65df7c09925c594f737be525a1
Georgii Shishkin <565-45@bk.ru>
2019-06-17 14:48:35 +0300

    4-01

--- src/part4/lesson01/task01/src/main/java/package1/Main.java
+++ src/part4/lesson01/task01/src/main/java/package1/Main.java
@@ -1,8 +1,20 @@
+package package1;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.sql.*;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 /**
+ * покрыть код логированием
+ * в основных блоках try покрыть уровнем INFO
+ * с исключениях catch покрыть уровнем ERROR
+ * настроить конфигурацию логера, что бы все логи записывались в БД, таблица LOGS,
+ * колонки ID, DATE, LOG_LEVEL, MESSAGE, EXCEPTION
+ *  *  *  *  *
  * 1)    Спроектировать базу
  * <p>
  * -      Таблица USER содержит поля id, name, birthday, login_ID, city, email, description
@@ -38,13 +50,13 @@ public class Main {
     static ResourceBundle resource = ResourceBundle.getBundle("demo");
     static Savepoint savepointB = null, savepointA = null;
     static ResultSet rs = null;
-
+    private static Logger logger = LogManager.getLogger(Main.class);
     /**
-     * метод для подключения к БД
+     * метод для подключения к БД. Также используется логгером.
      *
      * @return Connection
      */
-    static Connection getConnection() throws SQLException {
+  public static Connection getConnection() throws SQLException {
         String url = resource.getString("db.url");
         String user = resource.getString("db.user");
         String pass = resource.getString("db.password");
@@ -87,6 +99,7 @@ public class Main {
                 resource.getString("t2a.4"), resource.getString("t2a.5"), resource.getString("t2a.6"),
                 resource.getString("t2a.7"));
         ps.executeUpdate();
+        logger.info("Вставка с использованием параметризованного запроса");
     }
 
     /**
@@ -109,6 +122,7 @@ public class Main {
                 resource.getString("t2b.2.7"));
         ps.addBatch();
         ps.executeBatch();
+        logger.info("Вставка с использованием batch");
     }
 
     /**
@@ -125,6 +139,7 @@ public class Main {
         while (rs.next()) {
             System.out.println(rs.getString(2) + " " + rs.getString(1));
         }
+        logger.info("Выборка по login_ID и name");
     }
 
     /**
@@ -140,6 +155,7 @@ public class Main {
         connection.prepareStatement(resource.getString("t4a.ps2")).executeUpdate();
         savepointB = connection.setSavepoint(resource.getString("t4a.sp"));
         connection.prepareStatement(resource.getString("t4a.ps3")).executeUpdate();
+        logger.info("Операции \"(4a)\" проведены");
     }
 
     /**
@@ -154,6 +170,7 @@ public class Main {
         connection.prepareStatement(resource.getString("t4b.ps2")).executeUpdate();
         savepointA = connection.setSavepoint(resource.getString("t4b.sp"));
         connection.prepareStatement(resource.getString("t4b.ps3")).executeUpdate();
+        logger.info("Операции \"(4б)\" проведены");
     }
 
     /**
@@ -165,9 +182,11 @@ public class Main {
         boolean flag = false;
         try {
             connection = getConnection();
+            logger.info("Установлено соединение");
         } catch (SQLException e) {
             System.out.println(resource.getString("db.error"));
             flag = true;
+            logger.error("Ошибка при соединении с БД",e);
         }
         if (!flag) {
             try {
@@ -182,11 +201,18 @@ public class Main {
                 connection.commit();
             } catch (SQLException e) {
                 try {
-                    if (savepointA != null)
+                    if ((savepointA != null)&&(!connection.getAutoCommit()))
+                    {
+                        logger.error("Ошибка при выполнении \"(4б)\"",e);
                         connection.rollback(savepointA);
-                    connection.commit();
+                        connection.commit();
+                        logger.info("Успешный rollback");
+                        //e.printStackTrace();
+                    }
+
                 } catch (SQLException ex) {
-                    ex.printStackTrace();
+                    logger.error("Ошибка при rollback",ex);
+                    //ex.printStackTrace();
                 }
             } finally {
                 try {
@@ -195,8 +221,10 @@ public class Main {
                     if (ps != null)
                         ps.close();
                     connection.close();
+                    logger.info("Закрыто соединение");
                 } catch (SQLException e) {
-                    e.printStackTrace();
+                    logger.error("Ошибка при закрытии ресурсов",e);
+                    //e.printStackTrace();
                 }
             }
         }
